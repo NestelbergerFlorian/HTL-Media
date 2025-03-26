eb33b4ddbb292a23db60e352ca6adc6d2f6ccbdd
Schmoelzkese <flix.schmoelz@gmail.com>
2025-03-26 10:43:42 +0100

    added login/templates

--- CODE/htlMedia/htlMedia/login/views.py
+++ CODE/htlMedia/htlMedia/login/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def login(request):
-    return HttpResponse("This is the Login Page")
+ template = loader.get_template('login.html')
+ return HttpResponse(template.render())
