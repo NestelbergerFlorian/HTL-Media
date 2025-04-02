778a0489e4b35b4f46f04d44c2a221f2ef3729b9
Schmoelzkese <flix.schmoelz@gmail.com>
2025-04-02 10:30:09 +0200

    login django auth

--- CODE/htlMedia/htlMedia/login/urls.py
+++ CODE/htlMedia/htlMedia/login/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import login_view
+
 
 
 urlpatterns = [
-    path('login-test/', login_test_view, name='login_test'),
-    path('login/',views.login,name='login'),
+    path('login/', views.login_view, name='login_test'),
+    #path('login/',views.login,name='login'),
 ]
--- CODE/htlMedia/htlMedia/login/views.py
+++ CODE/htlMedia/htlMedia/login/views.py
@@ -5,15 +5,6 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 
-def login(request):
- template = loader.get_template('login.html')
- return HttpResponse(template.render())
-
-def login_test_view(request):
-    form = AuthenticationForm()
-    return render(request, 'login_test.html', {'form': form})
-
-
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -22,8 +13,9 @@ def login_view(request):
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
+                print('success')
                 login(request, user)
-                return redirect('home')  # Ersetzen Sie 'home' durch Ihre gewünschte Ziel-URL
+                return redirect('login')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
