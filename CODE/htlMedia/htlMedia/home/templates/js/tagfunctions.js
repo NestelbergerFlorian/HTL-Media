19f8d6a3ca732f1f61d91cb9b246cc29216f5418
NestelbergerFlorian <florian.nestelberger@htlwy.at>
2025-04-30 09:46:54 +0200

    improved responsive design for imgs and tags, added upload button

--- CODE/htlMedia/htlMedia/home/templates/js/tagfunctions.js
+++ CODE/htlMedia/htlMedia/home/templates/js/tagfunctions.js
@@ -1,15 +1,17 @@
-const tagflexid = "#tagimgwrapper";
-let tagflexiconid = "#tagpermaid";
+const tagflexid = "#htagimgwrapper";
+let tagflexiconid = "#htagpermaid";
 var tag = document.querySelector(tagflexid);
 
+
+
 function toggleTagDisplay(){
-    if(tag.classList.contains("tagflexhide")){
-        tag.classList.remove("tagflexhide");
+    if(tag.classList.contains("htagflexhide")){
+        tag.classList.remove("htagflexhide");
         document.querySelector(tagflexiconid).classList.remove("fa-chevron-right");
         document.querySelector(tagflexiconid).classList.add("fa-chevron-down");
     }
     else{
-        tag.classList.add("tagflexhide");
+        tag.classList.add("htagflexhide");
         document.querySelector(tagflexiconid).classList.remove("fa-chevron-down");
         document.querySelector(tagflexiconid).classList.add("fa-chevron-right");
     }
