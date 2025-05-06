41b31de3380f791b98210eb0ed971e203441acea
Jonas-Grestenberger <jonas.grestenberger@htlwy.at>
2025-05-06 14:43:30 +0200

    felix try this out please

--- CODE/htlMedia/htlMedia/htlMedia/urls.py
+++ CODE/htlMedia/htlMedia/htlMedia/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('',include('login.urls')),
     path('', RedirectView.as_view(url ='/login/',permanent=False),name='login'),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
