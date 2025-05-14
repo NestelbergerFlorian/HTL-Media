3fdf15332c250883ea2e52e3ad9c163bcfc998b9
Jonas-Grestenberger <jonas.grestenberger@htlwy.at>
2025-05-14 09:22:15 +0200

    AD Login feature complete --> home is the page where you get redirected after succesful login

--- CODE/htlMedia/htlMedia/login/urls.py
+++ CODE/htlMedia/htlMedia/login/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home/', views.home, name='home'),
     path('login/',views.login,name='login'),
+    path('authenticate_ldap/',views.authenticate_ldap,name='authenticate_ldap'),
 ]
--- CODE/htlMedia/htlMedia/login/views.py
+++ CODE/htlMedia/htlMedia/login/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from ldap3 import Server, Connection, ALL, SIMPLE
+from ldap3.core.exceptions import LDAPException
 
+from django.shortcuts import render 
 
 def login(request):
- template = loader.get_template('login.html')
- return HttpResponse(template.render())
+    print("Login view called")
+    return render(request, 'login.html')  # <-- das rendert mit RequestContext
+
+def home(request):
+    print("home view called")
+    return render(request, 'home.html')  # <-- das rendert mit RequestContex
+
+
+def authenticate_ldap(request):
+    print("Authenticate LDAP called")
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    LDAP_SERVER = 'ldaps://ldaps.htlwy.at'
+    BASE_DN = 'ou=users,dc=schule,dc=local'
+ 
+    user_dn = f'uid={username},{BASE_DN}'
+    server = Server(LDAP_SERVER, port=636, use_ssl=True, get_info=ALL)
+    print(f"Connecting to LDAP server: {LDAP_SERVER}")
+    
+    try:
+        conn = Connection(server, user=user_dn, password=password, authentication=SIMPLE, auto_bind=True)
+        print("Authenticated successfully.")
+        conn.unbind()
+        return redirect('home')  # Redirect to a success page or home page
+    except LDAPException as e:
+        print(f"Authentication failed: {e}")
+        return redirect('login')  # Redirect to login page with error message
