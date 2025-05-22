2491cb002ae4582cbd9236b79ed7006435faf00e
Jonas-Grestenberger <jonas.grestenberger@htlwy.at>
2025-05-22 08:56:46 +0200

    Upload functioning again

--- CODE/htlMedia/htlMedia/home/views.py
+++ CODE/htlMedia/htlMedia/home/views.py
@@ -5,17 +5,18 @@ from django.contrib.auth import logout
 from .froms import PostForm
 
 def home(request):
- if request.method == 'POST':
-  form = PostForm(request.POST, request.FILES)
-  if form.is_valid():
-   post = form.save(commit=False)
-   post.uploaded_by = request.user
-   post.uploaded_at = timezone.now()
-   form.save()
-   return redirect('success')
- else:
-   form = PostForm()
-   return render(request, 'home.html', {'form': form})
+  print("Hallo World")	
+  if request.method == 'POST':
+    form = PostForm(request.POST, request.FILES)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.uploaded_by = request.user
+      post.uploaded_at = timezone.now()
+      form.save()
+      return redirect('success')
+  else:
+    form = PostForm()
+    return render(request, 'home.html', {'form': form})
  
 def success(request):
  return HttpResponse('successfully uploaded')
--- CODE/htlMedia/htlMedia/login/urls.py
+++ CODE/htlMedia/htlMedia/login/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from home import views as home_views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('home/', home_views.home, name='home'),
     path('login/',views.login,name='login'),
     path('authenticate_ldap/',views.authenticate_ldap,name='authenticate_ldap'),
 ]
