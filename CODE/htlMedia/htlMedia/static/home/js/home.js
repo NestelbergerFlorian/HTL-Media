dbed025ad8e48d570e942711549e853c74f7558f
Schmoelzkese <flix.schmoelz@gmail.com>
2025-05-28 20:10:35 +0200

    added view selected picture

--- CODE/htlMedia/htlMedia/static/home/js/home.js
+++ CODE/htlMedia/htlMedia/static/home/js/home.js
@@ -18,12 +18,33 @@ function activateUploadform() {
     document.getElementById("form-container").style.display = "flex";
 }
 
-const hamburgerbutton = document.getElementById("hhamburger").addEventListener("click",(event) => {hamburgerClick()});
+function postClick(event){
+    let view = document.getElementById("view-container");
+    view.style.display = "flex";
+    view.appendChild(event.target.cloneNode(true));
+    
+}
+
+function disablePostview(event){
+    let view = event.target;
+    view.removeChild(view.lastElementChild);
+    view.style.display = "none";
+}
+
+const hamburgerbutton = document.getElementById("hhamburger").addEventListener("click",(event) => { hamburgerClick()});
 const backButton =      document.getElementById("backButton").addEventListener("click",(event) => {backButtonClick()});
 const upload =          document.getElementById("uploadButton").addEventListener("click",(event) => {activateUploadform()});
+const view =            document.getElementById("view-container").addEventListener("click",(event) => { disablePostview(event)})
+const posts =           document.getElementsByClassName("postPic");
+
+for (var i = 0; i < posts.length; i++) {
+    posts[i].addEventListener("click",(event) => {postClick(event)})
+}
+
+
 
 const DarkLightModeToggle = document.getElementById("DarkLightModeToggle");
 if (DarkLightModeToggle.checked) {
     const Html = document.getElementById("html"); 
-Html.setAttribute("data-theme", "light");
+    Html.setAttribute("data-theme", "light");
 }
